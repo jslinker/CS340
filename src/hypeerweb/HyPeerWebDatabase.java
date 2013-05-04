dc1a17cbacea2d0584ad702926853bdf91d8e5bc
Craig <craig.crj@gmail.com>
2013-05-04 16:20:52 -0600

    Renamed file. Added lib folder and 2 jar files. Added 2 methods to HyPeerWebDatabase.

--- src/hypeerweb/HyPeerWebDatabase.java
+++ src/hypeerweb/HyPeerWebDatabase.java
@@ -1,5 +1,50 @@
 package hypeerweb;
 
+/**
+ * 
+ * @author Craig Jacobson
+ *
+ */
 public class HyPeerWebDatabase {
 
+	public static final String DATABASE_DIRECTORY = "databases";
+	public static final String DEFAULT_DATABASE_NAME = "hypeerweb.db";
+	
+	private static HyPeerWebDatabase singleton = null;
+	
+	private HyPeerWebDatabase(String dbName){
+		
+	}
+	
+	/**
+	 * Creates and loads a HyPeerWebDatabase. 
+	 * Should be one of the first things called when creating a HyPeerWeb.
+	 * @param dbName The name of the database. Must be valid, null, or empty.
+	 * @Precondition dbName = null OR |dbName| = 0 OR dbName is valid
+	 * @Postcondition 
+	 * IF dbName == null OR |dbName| = 0
+	 * new HyPeerWebDatabase(DEFAULT_DATABASE_NAME)
+	 * ELSE
+	 * new HyPeerWebDatabase(dbName)
+	 * @author Craig Jacobson
+	 */
+	public static void initHyPeerWebDatabase(String dbName){
+		if(dbName == null || dbName.length() == 0)
+			singleton = new HyPeerWebDatabase(DEFAULT_DATABASE_NAME);
+		else
+			singleton = new HyPeerWebDatabase(dbName);
+	}
+	
+	/**
+	 * Gets the single HyPeerWebDatabase.
+	 * @return The HyPeerWebDatabase singleton.
+	 * @Precondition <code>initHyPeerWebDatabase()</code> must have been called previously to ensure
+	 * that result != null
+	 * @Postcondition result = singleton
+	 * @author Craig Jacobson
+	 */
+	public static HyPeerWebDatabase getSingleton(){
+		assert singleton != null;
+		return singleton;
+	}
 }
--- tests/hypeerweb/NodeTests.java
+++ tests/hypeerweb/NodeTests.java
@@ -1,4 +1,4 @@
-package unittests;
+package hypeerweb;
 
 public class NodeTests {
 
