2b95b1f0e040c9007d290cc7300c2dc57d3d9293
Craig <craig.crj@gmail.com>
2013-05-05 20:10:49 -0600

    Preliminary database design.
    Note that sqlite does not enforce foreign keys, they are included more
    as a description. Additionally, I didn't explicitly define a primary key
    so sqlite will automatically use the row id as the primary key. I did
    create unique values, which I consider more important.

--- db/HyPeerWeb_table_design.sql
+++ db/HyPeerWeb_table_design.sql
@@ -1,9 +1,39 @@
 CREATE TABLE IF NOT EXISTS nodes
 (
-	node_key INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	web_id INTEGER UNIQUE NOT NULL,
 	fold INTEGER,
 	surrogate_fold INTEGER,
-	FOREIGN KEY (fold) REFERENCES nodes(node_key),
-	FOREIGN KEY (surrogate_fold) REFERENCES nodes(node_key),
+	inverse_surrogate_fold INTEGER,
+	FOREIGN KEY (fold) REFERENCES nodes(web_id),
+	FOREIGN KEY (surrogate_fold) REFERENCES nodes(web_id),
+	FOREIGN KEY (inverse_surrogate_fold) REFERENCES nodes(web_id),
+);
+
+CREATE TABLE IF NOT EXISTS neighbors
+(
+	node INTEGER NOT NULL,
+	neighbor INTEGER NOT NULL,
+	FOREIGN KEY (node) REFERENCES nodes(web_id),
+	FOREIGN KEY (neighbor) REFERENCES nodes(web_id),
+	UNIQUE (node, neighbor)
+);
+
+--up pointers are the same as edge nodes
+CREATE TABLE IF NOT EXISTS up_pointers
+(
+	node INTEGER NOT NULL,
+	edge_node INTEGER NOT NULL,
+	FOREIGN KEY (node) REFERENCES nodes(web_id),
+	FOREIGN KEY (edge_node) REFERENCES nodes(web_id),
+	UNIQUE (node, edge_node)
+);
+
+--down pointers are the same as surrogate neighbors
+CREATE TABLE IF NOT EXISTS down_pointers
+(
+	node INTEGER NOT NULL,
+	surrogate_neighbor INTEGER NOT NULL,
+	FOREIGN KEY (node) REFERENCES nodes(web_id),
+	FOREIGN KEY (surrogate_neighbor) REFERENCES nodes(web_id),
+	UNIQUE (node, surrogate_neighbor)
 );
