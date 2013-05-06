7840d03be615c899b02ee7b50d1960ddb7b980db
jslinker <joseph.slinker@gmail.com>
2013-05-06 13:56:05 -0600

    Add getters and setters for Node class

--- src/hypeerweb/Node.java
+++ src/hypeerweb/Node.java
@@ -1,13 +1,24 @@
 package hypeerweb;
 
+import java.util.ArrayList;
+
 public class Node {
 	
 	private WebId webId = null;
+	private ArrayList<Node> downPointers = null;
+	private ArrayList<Node> upPointers = null;
+	private ArrayList<Node> neighbors = null;
+	private Node fold = null;
+	private Node surrogateFold = null;
+	private Node inverseSurrogateFold = null;
 	
 	public static final Node NULL_NODE = new Node();
 	
 	private Node(){
 		webId = WebId.NULL_WEB_ID;
+		downPointers = new ArrayList<Node>();
+		upPointers = new ArrayList<Node>();
+		neighbors = new ArrayList<Node>();
 	}
 
 	public Node(int id){
@@ -19,15 +30,15 @@ public class Node {
 	}
 	
 	public void addDownPointer(Node downPointer){
-		
+		this.downPointers.add(downPointer);
 	}
 	
 	public void addNeighbor(Node neighbor){
-		
+		this.neighbors.add(neighbor);
 	}
 	
 	public void addUpPointer(Node upPointer){
-		
+		this.upPointers.add(upPointer);
 	}
 	
 	public SimplifiedNodeDomain constructSimplifiedNodeDomain(){
@@ -39,30 +50,34 @@ public class Node {
 	}
 	
 	public void removeDownPointer(Node downPointer){
-		
+		this.downPointers.remove(downPointer);
 	}
 	
 	public void removeNeighbor(Node neighbor){
-		
+		this.neighbors.remove(neighbor);
 	}
 	
 	public void removeUpPointer(Node upPointer){
-		
+		this.upPointers.remove(upPointer);
 	}
 	
 	public void setFold(Node fold){
-		
+		this.fold = fold;
 	}
 	
 	public void setInverseSurrogateFold(Node inverseSurrogateFold){
-		
+		this.inverseSurrogateFold = inverseSurrogateFold;
 	}
 	
 	public void setSurrogateFold(Node surrogateFold){
-		
+		this.surrogateFold = surrogateFold;
 	}
 	
 	public void setWebId(WebId webId){
 		
+	}
+	
+	public boolean equals(Node otherNode){
+		return this.webId == otherNode.webId;
 	}
 }
